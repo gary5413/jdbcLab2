b7f007bc4ead7355b198a1602171df05d60b00a9
garylee <5413gary@gmail.com>
2023-12-22 16:39:17 +0800

    新增lab06metadata

--- src/jdbc_2/eeit179/garylee/lab/lab06metadata/DemoMetaData.java
+++ src/jdbc_2/eeit179/garylee/lab/lab06metadata/DemoMetaData.java
@@ -1,5 +1,7 @@
-package jdbc_2.eeit179.jerry.action;
+package jdbc_2.eeit179.garylee.lab.lab06metadata;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -7,14 +9,24 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class Demo8MetaData {
+
+
+import jdbc_2.eeit179.garylee.lab.util.JDBCutils;
+
+public class DemoMetaData {
+
 	private Connection conn;
-	public void createConnection() throws Exception{
+
+	public void CreateConnection() throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url="jdbc:mysql://localhost:3306/mytestdb";
-		String user="root";
-		String password="5413gary";
+		Properties properties = new Properties();
+		FileInputStream fileInputStream = new FileInputStream(new File("src/jdbc2.properties"));
+		properties.load(fileInputStream);
+		String url=properties.getProperty("url");
+		String user=properties.getProperty("user");
+		String password=properties.getProperty("password");
 		this.conn = DriverManager.getConnection(url,user,password);
 		boolean status = !conn.isClosed();
 		if(status) {
@@ -30,7 +42,7 @@ public class Demo8MetaData {
 	}
 	
 	public void testDatabaseMetaData() throws SQLException {
-		DatabaseMetaData metaData = conn.getMetaData();
+		DatabaseMetaData metaData= conn.getMetaData();
 		System.out.println("DatabaseProductName:"+metaData.getDatabaseProductName());
 		System.out.println("DatabaseProductName:"+metaData.getDatabaseProductVersion());
 		System.out.println("DriverName:"+metaData.getDriverName());
@@ -39,35 +51,43 @@ public class Demo8MetaData {
 	}
 	
 	public void testResultMetaData() throws SQLException {
-		String sql="SELECT * FROM customers";
-		PreparedStatement preState = conn.prepareStatement(sql);
-		ResultSet rs = preState.executeQuery();
-		ResultSetMetaData metaData = rs.getMetaData();
+		String sql="SELECT * FROM user";
+		PreparedStatement prepareStatement = conn.prepareStatement(sql);
+		ResultSet rs = prepareStatement.executeQuery();
+		ResultSetMetaData metaData= rs.getMetaData();
+//		回傳結果集的欄位數量
 		System.out.println("ColumnCount:"+metaData.getColumnCount());
+//		回傳欄位名稱/別名
 		System.out.println("ColumnLabel(1):"+metaData.getColumnLabel(1));
 		System.out.println("ColumnLabel(2):"+metaData.getColumnLabel(2));
+//		回傳SQL資料型別
 		System.out.println("ColumnTypeName(1):"+metaData.getColumnTypeName(1));
 		System.out.println("ColumnTypeName(2):"+metaData.getColumnTypeName(2));
+//		回傳欄位最大儲存尺寸
 		System.out.println("ColumnDisplaySize(1):"+metaData.getColumnDisplaySize(1));
 		System.out.println("ColumnDisplaySize(2):"+metaData.getColumnDisplaySize(2));
+/*
+ * 		傳回值: 
+		0 代表不允許NULL 
+		1 代表允許NULL 
+		2 代表無法判斷的情形 
+ */
+		System.out.println("isNullable(1):"+metaData.isNullable(1));
+		System.out.println("isNullable(5):"+metaData.isNullable(5));
+		System.out.println("isNullable(7):"+metaData.isNullable(7));
 		rs.close();
-		preState.close();
+		prepareStatement.close();
 	}
 	
+	
 	public static void main(String[] args) {
-		Demo8MetaData demo8 = new Demo8MetaData();
+		DemoMetaData demoMetaData = new DemoMetaData();
 		try {
-			demo8.createConnection();
-//			demo8.testDatabaseMetaData();
-			demo8.testResultMetaData();
+			demoMetaData.CreateConnection();
+//			demoMetaData.testDatabaseMetaData();
+			demoMetaData.testResultMetaData();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			try {
-				demo8.closeConnection();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
